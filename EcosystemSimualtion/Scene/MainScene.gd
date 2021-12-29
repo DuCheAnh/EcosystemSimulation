91c911bdba140405890b61cf7ea9dbe6d0525aa6
thichuong <18520539@gm.uit.edu.vn>
2021-12-29 14:41:31 +0700

    add food

--- EcosystemSimualtion/Scene/MainScene.gd
+++ EcosystemSimualtion/Scene/MainScene.gd
@@ -3,13 +3,23 @@ extends Node2D
 var chicken_preload = preload("res://Scene/Chicken.tscn")
 var food_preload = preload("res://Scene/Food.tscn")
 
+export (float) var Add_Food_tick_time = 10
+export (bool) var Add_Food = false
+
 onready var chickens = $Chickens
 onready var foods = $Foods
+onready var Add_Food_timer = $AddFoodTimer
 
 func _ready():
 	randomize()
+	Add_Food_timer.wait_time = Add_Food_tick_time
 	add_chicken(10)
 	add_food(30)
+
+func _process(delta) -> void:
+	if Add_Food:
+		add_food(1)
+		Add_Food = false
 
 
 func add_food(amount) -> void:
@@ -25,3 +35,8 @@ func add_chicken(amount) -> void:
 		var chicken_ins = chicken_preload.instance()
 		chicken_ins.position = Vector2(rand_range(0,screen_size.x),rand_range(0,screen_size.y))
 		chickens.add_child(chicken_ins)
+
+
+func _on_AddFoodsTimer_timeout() -> void:
+		Add_Food = true
+
