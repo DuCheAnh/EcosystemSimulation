665f23bd8ac8682022d768b5930dc8da646ff90d
DuCheAnh <ducheanh1308@gmail.com>
2022-01-11 08:21:06 +0700

    final report

--- EcosystemSimualtion/Chicken.gd
+++ EcosystemSimualtion/Chicken.gd
@@ -1,17 +1,20 @@
 extends Area2D
 
-export (float) var speed = 150
-export (int) var hunger = 100
-export (int) var hunger_threshold = 40
-export (int) var hunger_decrement = 5
-export (float, 0, 1, 0.1) var thrive_for_food = 0.1
-export (int) var min_hunger = 0
-export (int) var max_hunger = 100
-export (float) var eat_time = 2
-export (float) var hunger_tick_time = 2
-export (float, 0, 1, 0.1) var reproductive_thrive = 0
+export (float) var speed
+export (int) var hunger
+export (int) var hunger_threshold
+export (int) var hunger_decrement
+export (int) var min_hunger
+export (int) var max_hunger
+export (float) var eat_time
+export (float) var hunger_tick_time
+export (float, 0, 1, 0.1) var reproductive_thrive
+export (float, 0, 1, 0.1) var reproductive_thrive_threshold
+export (int) var detection_range
 
-
+var genes = []
+export (float) var age = 0
+export (int) var portion = 50
 onready var screen_width = get_viewport_rect().size.x
 onready var screen_height = get_viewport_rect().size.y
 onready var direction_timer = $DirectionTimer
@@ -36,17 +39,49 @@ var target_food = null
 var target_mate = null
 var state = SEARCHING_FOOD
 
+func apply_genes(new_genes):
+	genes = new_genes
+	speed = genes[0]
+	hunger = genes[1]
+	hunger_threshold = genes[2]
+	hunger_decrement = genes[3]
+	min_hunger = genes[4]
+	max_hunger = genes[5]
+	eat_time = genes[6]
+	hunger_tick_time = genes[7]
+	reproductive_thrive = genes[8]
+	reproductive_thrive_threshold = genes[9]
+	detection_range = genes[10]
+
+func create_new_genes(partners_genes):
+	var arr = []
+	for i in range(genes.size()):
+		if rand_range(0,1)<0.5:
+			arr.append(genes[i])
+		else:
+			arr.append(partners_genes[i])
+		if rand_range(0,1)<0.2:
+			if rand_range(0,1)<0.5:
+				arr[i]+=arr[i]*rand_range(0, 0.2)
+			else:
+				arr[i]-=arr[i]*0.1*rand_range(0, 0.2)
+	return arr
 enum {
 	SEARCHING_FOOD,
 	FOUND_FOOD,
+	FOUND_MATE,
 	EATING,
+	MATING,
 	ENJOYING_LIFE
 }
+func eaten():
+	queue_free()
 func _ready() -> void:
 	randomize()
 	direction = _new_direction()
 	hunger_timer.wait_time = hunger_tick_time
 	eat_timer.wait_time = eat_time
+	$DetectionRange.shape.radius = detection_range
 
 func _process(delta) -> void:
 	match state:
@@ -54,15 +89,19 @@ func _process(delta) -> void:
 			move_randomly(delta)
 		FOUND_FOOD:
 			go_to_food(delta)
-
+		FOUND_MATE:
+			go_to_food(delta)
 	update_label()
 	update_state()
-
+	if starved() or age >= 6:
+		queue_free()
 func update_state() -> void:
 	if hungry():
 		sprite.modulate = Color(1, 0.5, 0.5)
+	elif horny():
+		sprite.modulate = Color(0.5,1,1)
 	else:
-		sprite.modulate = Color(1,1,1)
+		sprite.modulate = Color(1, 1, 1)
 
 
 func update_label() -> void:
@@ -72,14 +111,18 @@ func update_label() -> void:
 			state_string = "SEARCHING"
 		FOUND_FOOD:
 			state_string = "FOUND FOOD"
+		MATING:
+			state_string = "MATING"
 		EATING:
 			state_string = "EATING"
-		ENJOYING_LIFE:
-			state_string = "ENJOYING LIFE"
-	status_label.text = "HUNGER: " + str(hunger) + "/" + str(max_hunger)
-	status_label.text += "\nDIRECTION: " + str(direction)
+		FOUND_MATE:
+			state_string = "FOUNDMATE"
+	status_label.text = "AGE: " + str(age)
+	status_label.text += "\nHUNGER: " + str(int(max_hunger-hunger)) + "/" + str(int(max_hunger))
+	status_label.text += "\nSPEED: " + str(int(speed))
 	status_label.text += "\n STATE: " + str(state_string)
-	status_label.text += "\n THRIVE: " + str(reproductive_thrive)
+	status_label.text += "\n THRIVE: " + str(reproductive_thrive) + "/" + str(reproductive_thrive_threshold)
+	status_label.text += "\n RANGE: " + str(detection_range)
 
 func move_randomly(delta) -> void:
 	_rotate_sprite()
@@ -90,6 +133,9 @@ func go_to_food(delta):
 	if is_instance_valid(target_food):
 		$Sprite.rotation_degrees = rad2deg(target_food.position.angle_to_point(position)) + 90
 		position += position.direction_to(target_food.position) * speed * delta
+	elif is_instance_valid(target_mate):
+		$Sprite.rotation_degrees = rad2deg(target_mate.position.angle_to_point(position)) + 90
+		position += position.direction_to(target_mate.position) * speed * delta
 	else:
 		state = SEARCHING_FOOD
 
@@ -99,6 +145,8 @@ func _keep_sprite_on_screen() -> void:
 		direction.x *= -1
 	if position.y <= 0 || position.y >= screen_height:
 		direction.y *= -1
+	position.x = clamp(position.x,0,screen_width)
+	position.y = clamp(position.y,0,screen_height)
 
 func _rotate_sprite() -> void:
 	if direction == Vector2.ZERO:
@@ -114,44 +162,61 @@ func _choose(options):
 	options.shuffle()
 	return options.front()
 
-
-func _on_DirectionTimer_timeout() -> void:
-	var time = _choose([1, 1.5, 2])
-	direction_timer.set_wait_time(time)
-	direction = _new_direction()
-
-
-func _on_HungerTimer_timeout() -> void:
-	hunger = clamp(hunger - hunger_decrement, min_hunger, max_hunger)
-	reproductive_thrive = clamp(reproductive_thrive + 0.1, 0, 1)
-
 func hungry() -> bool:
 	return hunger < hunger_threshold
 
+func starved() -> bool:
+	return hunger == 0
+func stop_moving():
+	speed = 0
 func horny() ->bool:
-	return reproductive_thrive > 0.5
+	return reproductive_thrive > reproductive_thrive_threshold
 
-func want_a_full_belly() -> bool:
-	return rand_range(0,1) <= thrive_for_food
 func _on_Chicken_area_entered(area):
 	if is_instance_valid(target_food):
 		return
-	if area.is_in_group("Food") and (hungry() or want_a_full_belly()):
-		state = FOUND_FOOD
-		target_food = area
-
-
+	if is_instance_valid(target_mate):
+		return
+	if area.is_in_group("Wolf"):
+		direction = area.direction
+		return
+	if hungry():
+		if area.is_in_group("Food"):
+			state = FOUND_FOOD
+			target_food = area
+	elif horny():
+		if area.is_in_group("Chicken") and area.horny() and not area.hungry():
+			state = FOUND_MATE
+			target_mate = area
 
 
 func _on_EatingArea_area_entered(area):
 	if is_instance_valid(target_food):
 		state = EATING
 		eat_timer.start()
+	elif is_instance_valid(target_mate):
+		state = MATING
+		eat_timer.start()
 
+func _on_DirectionTimer_timeout() -> void:
+	var time = _choose([1, 1.5, 2])
+	direction_timer.set_wait_time(time)
+	direction = _new_direction()
+
+func _on_HungerTimer_timeout() -> void:
+	hunger = clamp(hunger - hunger_decrement, min_hunger, max_hunger)
+	reproductive_thrive = clamp(reproductive_thrive + 0.1, 0, 100)
+	age+=0.1
 
 func _on_EatTimer_timeout():
 	if is_instance_valid(target_food):
 		hunger += target_food.portion
 		target_food.eaten()
+	elif is_instance_valid(target_mate):
+		var baby_genes = create_new_genes(target_mate.genes)
+		get_parent().get_parent().add_baby_at_pos(baby_genes, position)
+		reproductive_thrive = 0
+		target_mate = null
 	eat_timer.stop()
 	state = SEARCHING_FOOD
+
--- EcosystemSimualtion/Scene/MainScene.gd
+++ EcosystemSimualtion/Scene/MainScene.gd
@@ -2,15 +2,92 @@ extends Node2D
 
 var chicken_preload = preload("res://Scene/Chicken.tscn")
 var food_preload = preload("res://Scene/Food.tscn")
-
+var wolf_preload = preload("res://Scene/Wolf.tscn")
 onready var chickens = $Chickens
 onready var foods = $Foods
-
+onready var animals_data = $Chickens
+var food_amount = 5
+var content = ""
+var day_passed = 0
+var basic_genes = [150, 100, 40, 5, 0, 100, 2, 3, 0, 1, 80]
+var wolf_basic_genes = [200, 100, 60, 5, 0, 100, 2, 3, 0, 1, 200]
 func _ready():
 	randomize()
 	add_chicken(10)
+	add_wolf(3)
 	add_food(30)
+func average_speed(Chickens):
+	if Chickens.get_child_count()<1:
+		return 0
+	var sum = 0
+	for chicken in Chickens.get_children():
+		sum += chicken.speed
+	return sum/Chickens.get_child_count()
+func average_range(Chickens):
+	if Chickens.get_child_count()<1:
+		return 0
+	var sum = 0
+	for chicken in Chickens.get_children():
+		sum += chicken.detection_range
+	return sum/Chickens.get_child_count()
+func average_age(Chickens):
+	if Chickens.get_child_count()<1:
+		return 0
+	var sum = 0
+	for chicken in Chickens.get_children():
+		sum += chicken.age
+	return sum/Chickens.get_child_count()
 
+func average_max_hunger(Chickens):
+	if Chickens.get_child_count()<1:
+		return 0
+	var sum = 0
+	for chicken in Chickens.get_children():
+		sum += chicken.max_hunger
+	return sum/Chickens.get_child_count()
+
+func average_hunger_decrement(Chickens):
+	if Chickens.get_child_count()<1:
+		return 0
+	var sum = 0
+	for chicken in Chickens.get_children():
+		sum += chicken.hunger_decrement
+	return sum/Chickens.get_child_count()
+func average_reproductive_thrive_threshold(Chickens):
+	if Chickens.get_child_count()<1:
+		return 0
+	var sum = 0
+	for chicken in Chickens.get_children():
+		sum += chicken.reproductive_thrive_threshold
+	return sum/Chickens.get_child_count()
+func update_status_board_of(animals):
+	$Label.text = "Days passed: "+ str(day_passed) + "\n"
+	$Label.text += "Amount: " + str(animals.get_child_count())
+	$Label.text += "\nAverage age: " + str(average_age(animals))
+	$Label.text += "\nAverage speed: " + str(average_speed(animals))
+	$Label.text += "\nAverage range: " + str(average_range(animals))
+	$Label.text += "\nAverage hunger threshold: " + str(average_max_hunger(animals))
+	$Label.text += "\nAverage hunger decrement: " + str(average_hunger_decrement(animals))
+	$Label.text += "\nAverage repro threshold: " + str(average_reproductive_thrive_threshold($Chickens)) + "\n"
+func get_data(animals):
+	var tx = ""
+	tx = str(day_passed) + "	"
+	tx += str(animals.get_child_count())
+	tx += "	" + str(average_age(animals))
+	tx += "	" + str(average_speed(animals))
+	tx += "	" + str(average_range(animals))
+	tx += "	" + str(average_max_hunger(animals))
+	tx += "	" + str(average_hunger_decrement(animals))
+	tx += "	" + str(average_reproductive_thrive_threshold($Chickens)) + "\n"
+	return tx
+func save(content):
+	var file = File.new()
+	file.open("res://saved_data.txt", File.WRITE)
+	file.store_string(content)
+	file.close()
+func _process(delta):
+	update_status_board_of(animals_data)
+	$Control/SpeedLabel.text = str(Engine.time_scale)
 
 func add_food(amount) -> void:
 	var screen_size = get_viewport_rect().size
@@ -23,5 +100,78 @@ func add_chicken(amount) -> void:
 	var screen_size = get_viewport_rect().size
 	for i in range(amount):
 		var chicken_ins = chicken_preload.instance()
+		chicken_ins.apply_genes(basic_genes)
 		chicken_ins.position = Vector2(rand_range(0,screen_size.x),rand_range(0,screen_size.y))
 		chickens.add_child(chicken_ins)
+func add_wolf(amount) -> void:
+	var screen_size = get_viewport_rect().size
+	for i in range(amount):
+		var wolf_ins = wolf_preload.instance()
+		wolf_ins.apply_genes(wolf_basic_genes)
+		wolf_ins.position = Vector2(rand_range(0,screen_size.x),rand_range(0,screen_size.y))
+		$Wolves.add_child(wolf_ins)
+
+func add_baby_at_pos(genes, pos):
+	if rand_range(0,1)<0.3:
+		return
+	var chicken_ins = chicken_preload.instance()
+	chicken_ins.apply_genes(genes)
+	chicken_ins.position = pos
+	chickens.add_child(chicken_ins)
+func add_cup_at_pos(genes, pos):
+	if rand_range(0,1)<0.5:
+		return
+	var wolf_ins = wolf_preload.instance()
+	wolf_ins.apply_genes(genes)
+	wolf_ins.position = pos
+	$Wolves.add_child(wolf_ins)
+
+
+func _on_FoodTimer_timeout():
+	add_food(food_amount)
+	content+=get_data($Chickens)
+	save(content)
+	day_passed += 0.5
+
+
+func _on_AddWolfBtn_pressed():
+	add_wolf(1)
+
+
+func _on_AddChickenBtn_pressed():
+	add_chicken(1)
+
+
+func _on_IncreaseSpeedBtn_pressed():
+	Engine.time_scale+=1
+
+
+func _on_DecreaseSpeedBtn_pressed():
+	Engine.time_scale = clamp(Engine.time_scale - 1, 0,10000)
+
+
+func _on_ShowCDataBtn_pressed():
+	animals_data = $Chickens
+
+func _on_ShowCDataBtn2_pressed():
+	animals_data = $Wolves
+
+
+func _on_RMChickenBtn_pressed():
+	for chicken in $Chickens.get_children():
+		chicken.queue_free()
+
+
+func _on_RMWolfBtn_pressed():
+	for wolf in $Wolves.get_children():
+		wolf.queue_free()
+
+
+func _on_SetFoodBtn_pressed():
+	food_amount = int($Control/FoodAmountTE.text)
+
+
+
+
+func _on_ResetButton_pressed():
+	Engine.time_scale = 1
